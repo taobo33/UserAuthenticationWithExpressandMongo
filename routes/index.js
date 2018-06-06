549c146c712040795f86b24aab2467964e43beb0
Bozhong Tao <3196115@qq.com>
2018-06-07 02:37:39 +0800

    First commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+// GET/Register
+router.get('/register', function(req, res, next) {
+    return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /Register
+router.post('/register', function(req, res, next) {
+    return res.send('User created!');
+});
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
